f99bc7d90a00d7a6f458368efac3c879b8581ed6
kyo1941 <138639762+kyo1941@users.noreply.github.com>
2025-04-05 23:07:57 +0900

    C問題を追加

---
+++ ABC400/C.cpp
@@ -0,0 +1,35 @@
+// 提出コード
+
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define ALL(a) (a).begin(), (a).end()
+typedef long long ll;
+
+int main() {
+    ll n;
+    cin >> n;
+
+    ll a = 1, ans = 0;
+    while (a * 2 <= n) {
+        a *= 2;
+
+        ll b_limit = n / a;
+        ll lo = -1, hi = n;
+        while (lo + 1 < hi) {
+            ll mid = lo + (hi - lo) / 2;
+
+            if (mid == 0 || mid <= b_limit / mid)
+                lo = mid;
+            else
+                hi = mid;
+        }
+        ll b = lo + 1;
+
+        ans += b / 2;
+    }
+
+    cout << ans << endl;
+}
